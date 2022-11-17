e300af7ea72ba2e0324498d2638ba02ea2c42b41
Rehan Khalil <rehan.khalil@IdexJo.Local>
2022-11-17 15:06:17 +0400

    make it better

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
-import bodyParser from "body-parser";
+// import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import wordRoutes from "./routes/wordsRoutes.js";
+dotenv.config();
 
 const app = express();
-dotenv.config({ path: ".env" });
 
-app.use(bodyParser.json({ limit: { word: "30mb" }, extended: true }));
-app.use(bodyParser.urlencoded({ limit: { word: "30mb" }, extended: true }));
 app.use(cors());
 app.use("/words", wordRoutes);
 
