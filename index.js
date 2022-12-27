70cacb81aa244714a51a2d402989c6a0a35bdf07
Rehan Khalil <bcs.f17.m.46.b@gmail.com>
2022-12-27 16:55:01 +0400

    renamed entrypoint response

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use("/words", wordRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Hello to the Wordle React Native Server !");
+  res.send("Hello to the Wordle Backend Server !");
 });
 
 const PORT = process.env.PORT || 5000;
